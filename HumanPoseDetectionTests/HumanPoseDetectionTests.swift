2591a3c24acbbc6548de1bc7f1b192274947ff77
cole schaffer <coleschaffer6@gmail.com>
2025-04-14 04:58:31 -0400

    Initial Commit

---
+++ HumanPoseDetectionTests/HumanPoseDetectionTests.swift
@@ -0,0 +1,17 @@
+//
+//  HumanPoseDetectionTests.swift
+//  HumanPoseDetectionTests
+//
+//  Created by Cole Schaffer on 4/14/25.
+//
+
+import Testing
+@testable import HumanPoseDetection
+
+struct HumanPoseDetectionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
